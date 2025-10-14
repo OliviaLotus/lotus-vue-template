eceb0bfab6591d0abf2da69da502dc4bb71f9bfc
OliviaLotus <qianseyear@gmail.com>
2025-10-14 13:40:57 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,13 @@ import App from "./App.vue";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import router from "./router";
-import { createPinia } from "pinia";
+import pinia from "./store";
 
-const pinia = createPinia();
 const app = createApp(App);
 
-app.use(router);
-app.use(pinia);
-app.use(ElementPlus);
-app.mount("#app");
+// 自定义指令
+import directives from "@/directives";
+Object.entries(directives).forEach(([name, dir]) => app.directive(name, dir));
+
+// 注册配置
+app.use(router).use(pinia).use(ElementPlus).mount("#app");
